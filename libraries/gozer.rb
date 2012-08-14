47b05f692b5c05d973782ba1b74a9cf2918ffefd
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-14 08:28:15 -0700

    Fix local variable name

--- libraries/gozer.rb
+++ libraries/gozer.rb
@@ -12,12 +12,12 @@ module Gozer
     def bag(bag_name, item_name, secret)
       begin
         if(secret)
-          Chef::EncryptedDataBagItem.load(bag_name, bag_item, bag_secret(secret)).to_hash
+          Chef::EncryptedDataBagItem.load(bag_name, item_name, bag_secret(secret)).to_hash
         else
           Chef::DataBagItem.load(bag_name, item_name).to_hash
         end
       rescue => e
-        Chef::Log.info "Failed to retreive item: #{bag_item} from data bag: #{bag_name}"
+        Chef::Log.info "Failed to retreive item: #{item_name} from data bag: #{bag_name}"
         Chef::Log.debug "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
       end
     end
