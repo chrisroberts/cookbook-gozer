f8f81b927943b9bb9470080953c8764086d5b897
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-17 10:19:09 -0700

    Add i3 configurations

--- recipes/i3.rb
+++ recipes/i3.rb
@@ -1,1 +1,9 @@
 package 'i3'
+
+remote_directory "/home/#{node[:gozer][:username]}/.i3" do
+  source "i3"
+  action :create
+  owner node[:gozer][:username]
+  group node[:gozer][:username]
+  files_mode 0644
+end
