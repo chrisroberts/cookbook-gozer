d4916763aeff2640919c782413d2c0a55c7e485e
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-23 07:28:19 -0700

    Infect vim

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -7,4 +7,21 @@ cookbook_file "/home/#{node[:gozer][:username]}/.vimrc" do
   group node[:gozer][:username]
 end
 
+package 'git'
 
+%w(.vim .vim/bundle .vim/autoload).each do |dir|
+  directory "/home/#{node[:gozer][:username]}/#{dir}" do
+    action :create
+    mode 0755
+    owner node[:gozer][:username]
+    group node[:gozer][:username]
+  end
+end
+
+remote_file "/home/#{node[:gozer][:username]}/.vim/autoload/pathogen.vim" do
+  action :create_if_missing
+  source 'https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
+  mode 0644
+  owner node[:gozer][:username]
+  group node[:gozer][:username]
+end
