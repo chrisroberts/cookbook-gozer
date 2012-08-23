24615392478d6293b1d2543e515a0db1676ff1c8
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-23 10:40:53 -0700

    Bundle some stuff for vim

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -25,3 +25,20 @@ remote_file "/home/#{node[:gozer][:username]}/.vim/autoload/pathogen.vim" do
   owner node[:gozer][:username]
   group node[:gozer][:username]
 end
+
+node[:gozer][:vim][:bundles].each do |bundle_repo|
+  execute "vim -> clone #{bundle_repo}" do
+    command "git clone #{bundle_repo}"
+    cwd "/home/#{node[:gozer][:username]}/.vim/bundle"
+    user node[:gozer][:username]
+    group node[:gozer][:username]
+    not_if do
+      File.directory?(
+        File.join(
+          "/home/#{node[:gozer][:username]}/.vim/bundle",
+          File.basename(bundle_repo).sub('.git', '')
+        )
+      )
+    end
+  end
+end
