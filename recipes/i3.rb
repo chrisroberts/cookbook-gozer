8c33359b173f1dcee6c1cfd9b5d34f0bd8cba365
Chris Roberts <chrisroberts.code@gmail.com>
2012-08-23 10:38:41 -0700

    Remove deprecated recipe

--- recipes/i3.rb
+++
@@ -1,9 +0,0 @@
-package 'i3'
-
-remote_directory "/home/#{node[:gozer][:username]}/.i3" do
-  source "i3"
-  action :create
-  owner node[:gozer][:username]
-  group node[:gozer][:username]
-  files_mode 0644
-end
