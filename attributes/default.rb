1119b60fd616f261cbf9207450102bcef5836c06
Chris Roberts <chrisroberts.code@gmail.com>
2012-09-04 18:41:22 -0700

    New attributes for github and backups

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,12 @@ default[:gozer][:packages] = %w(tmux moc)
 default[:gozer][:hostname] = 'gozer'
 default[:gozer][:username] = 'spox'
 default[:gozer][:skype_package] = 'http://download.skype.com/linux/skype-ubuntu_4.0.0.8-1_amd64.deb'
+default[:gozer][:github][:accounts] = []
+default[:gozer][:github][:exclude] = {}
+default[:gozer][:github][:allow_forks] = {}
 default[:gozer][:projects][:git] = {}
 default[:gozer][:vim][:bundles] = %w(
   git://github.com/altercation/vim-colors-solarized.git
   git://github.com/elixir-lang/vim-elixir.git
 )
+default[:gozer][:backup][:uuid] = nil
