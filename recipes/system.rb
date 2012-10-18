2c5ee0305fd95f3d845e514009f0e6209ed73c64
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-18 10:00:29 -0700

    Upgrade!

--- recipes/system.rb
+++ recipes/system.rb
@@ -2,3 +2,9 @@ file '/etc/hostname' do
   content node[:gozer][:hostname]
   mode 0644
 end
+
+execute 'go go gadget upgrade' do
+  command 'apt-get -q -y upgrade'
+  action :nothing
+  subscribes :run, resources(:execute => 'apt-get-update-periodic'), :delayed
+end
