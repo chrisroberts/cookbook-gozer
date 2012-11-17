80889b1940b6a3314141ca16febe2ea172060474
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-16 17:34:03 -0800

    Add git-core ppa. Install subtree

--- recipes/git.rb
+++ recipes/git.rb
@@ -1,5 +1,26 @@
+include_recipe 'apt'
+
+execute 'add git ppa repo' do
+  command 'add-apt-repository -y ppa:git-core/ppa'
+  not_if 'apt-cache policy | grep git-core'
+  notifies :run, resources(:execute => 'apt-get update'), :immediately
+end
+
 package 'git'
 package 'git-flow'
+
+script 'git[install subtree]' do
+  action :nothing
+  user node[:gozer][:username]
+  code <<-EOH
+    [ ! -e '~/src/git-core' ] && mkdir -p ~/src/git-core
+    cd ~/src/git-core && apt-get source git-core
+    cd ~/src/git-core/git-*/contrib/subtree/
+    [ -e '/usr/lib/git-core' ] && sed -i -e '/^libexecdir.*/ s|/libexec/|/lib/|' Makefile || echo '/usr/lib/git-core does not exist! Check that your libexec dir exists and reinstall git-subtree'
+    sudo make prefix=/usr && sudo make prefix=/usr install && sudo make prefix=/usr install-doc
+  EOH
+  subscribes :run, resources(:package => 'git')
+end
 
 config_path = "/home/#{node[:gozer][:username]}/.gitconfig"
 
